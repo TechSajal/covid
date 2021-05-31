a54e60bf214b60543d765b4c0523387e072b26a1
Sajal Kaushal <sajalkaushal3@gmil.com>
2021-06-01 02:06:03 +0530

    covid app world data and india data completed

--- app/src/main/java/com/example/covid/MainActivity.kt
+++ app/src/main/java/com/example/covid/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.covid
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -13,8 +17,11 @@ import org.eazegraph.lib.models.PieModel
 import org.json.JSONArray
 import org.json.JSONObject
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MainActivity : AppCompatActivity() {
 
     private var confirmed: String? = null
@@ -28,7 +35,6 @@ class MainActivity : AppCompatActivity() {
     private  var tests:String? = null
     private  var tests_new:String? = null
     private  var time:String? = null
-    private var int_active_new = 0
     private lateinit var con:TextView
     private lateinit var connew:TextView
     private lateinit var act:TextView
@@ -39,71 +45,104 @@ class MainActivity : AppCompatActivity() {
     private lateinit var deanew:TextView
     private lateinit var tes:TextView
     private lateinit var tesnew:TextView
+    private lateinit var date:TextView
+    private lateinit var tim:TextView
     private lateinit var pieChart: PieChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val queue =Volley.newRequestQueue(this)
-        val url = "https://api.covid19india.org/data.json"
-        val jsonObjectRequest = JsonObjectRequest(
-                                     Request.Method.GET,
-                                     url,
-                                    null,
-            { response ->
-                     var allstatejsonArray:JSONArray? = null
-                     var testdatasome:JSONArray? = null
-                      allstatejsonArray = response.getJSONArray("statewise")
-                      testdatasome = response.getJSONArray("tested")
-                      val data_india:JSONObject = allstatejsonArray.getJSONObject(0)
-                      val test_data_india:JSONObject = testdatasome.getJSONObject(testdatasome.length()-1)
-                      confirmed=data_india.getString("confirmed")
-                     confirmed_new = data_india.getString("deltaconfirmed")
-                     active = data_india.getString("active")
-                     recovered = data_india.getString("recovered")
-                     recovered_new = data_india.getString("deltarecovered")
-                     death = data_india.getString("deaths")
-                     death_new = data_india.getString("deltadeaths")
-                     time = data_india.getString("lastupdatedtime")
-                     tests = test_data_india.getString("totalsamplestested")
-                     tests_new = test_data_india.getString("samplereportedtoday")
-                    active_new = (Integer.parseInt(confirmed_new) -(Integer.parseInt(death_new) + Integer.parseInt(recovered_new))).toString()
-                   val delayToshowProcess = Handler()
-                     delayToshowProcess.postDelayed({
-                          con = findViewById(R.id.confirm)
-                          connew = findViewById(R.id.confirmednew)
-                          rec = findViewById(R.id.recovered)
-                         recnew = findViewById(R.id.recoverednew)
-                         act = findViewById(R.id.active)
-                         actnew = findViewById(R.id.activenew)
-                         dea = findViewById(R.id.death)
-                         deanew = findViewById(R.id.deathnew)
-                         tes = findViewById(R.id.sampletested)
-                         tesnew = findViewById(R.id.sampletestednew)
-                         con.text = NumberFormat.getInstance().format(Integer.parseInt(confirmed))
-                         connew.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(confirmed_new)))
-                         rec.text = NumberFormat.getInstance().format(Integer.parseInt(recovered))
-                         recnew.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(recovered_new)))
-                         dea.text = NumberFormat.getInstance().format(Integer.parseInt(death))
-                         deanew.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(death_new)))
-                         tes.text = NumberFormat.getInstance().format(Integer.parseInt(tests))
-                         tesnew.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(tests_new)))
-                         act.text = NumberFormat.getInstance().format(Integer.parseInt(active))
-                         actnew.text = NumberFormat.getInstance().format(Integer.parseInt(active_new))
-                         pieChart= findViewById(R.id.activity_main_piechart)
-                         pieChart.addPieSlice(PieModel("Active",
-                             Integer.parseInt(active).toFloat(), Color.parseColor("#007afe")))
-                         pieChart.addPieSlice(PieModel("Recovered", Integer.parseInt(recovered).toFloat(), Color.parseColor("#08a045")))
-                         pieChart.addPieSlice(PieModel("Deceased", Integer.parseInt(death).toFloat(), Color.parseColor("#F6404F")))
-                         pieChart.startAnimation()
+        supportActionBar!!.title = "Covid-19 Tracker (India)"
+          FetchData()
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipe_refresh_layout)
+        swipeRefreshLayout.setOnRefreshListener {
+                  FetchData()
+            swipeRefreshLayout.isRefreshing = false
+        }
 
+          val worlddatabutton = findViewById<LinearLayout>(R.id.activity_main_world_data_lin)
+           worlddatabutton.setOnClickListener {
+               val intent = Intent(this,WorldDataActivity::class.java)
+               startActivity(intent)
+               finish()
+           }
+    }
+        fun FetchData() {
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://api.covid19india.org/data.json"
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                { response ->
+                    val allstatejsonArray: JSONArray = response.getJSONArray("statewise")
+                    val testdatasome: JSONArray = response.getJSONArray("tested")
+                    val data_india: JSONObject = allstatejsonArray.getJSONObject(0)
+                    val test_data_india: JSONObject = testdatasome.getJSONObject(testdatasome.length() - 1)
+                    confirmed = data_india.getString("confirmed")
+                    confirmed_new = data_india.getString("deltaconfirmed")
+                    active = data_india.getString("active")
+                    recovered = data_india.getString("recovered")
+                    recovered_new = data_india.getString("deltarecovered")
+                    death = data_india.getString("deaths")
+                    death_new = data_india.getString("deltadeaths")
+                    time = data_india.getString("lastupdatedtime")
+                    tests = test_data_india.getString("totalsamplestested")
+                    tests_new = test_data_india.getString("samplereportedtoday")
+                    active_new =
+                        (Integer.parseInt(confirmed_new) - (Integer.parseInt(death_new) + Integer.parseInt(
+                            recovered_new))).toString()
+                    val delayToshowProcess = Handler()
+                    delayToshowProcess.postDelayed({
+                        con = findViewById(R.id.confirm)
+                        connew = findViewById(R.id.confirmednew)
+                        rec = findViewById(R.id.recovered)
+                        recnew = findViewById(R.id.recoverednew)
+                        act = findViewById(R.id.active)
+                        actnew = findViewById(R.id.activenew)
+                        dea = findViewById(R.id.death)
+                        deanew = findViewById(R.id.deathnew)
+                        tes = findViewById(R.id.sampletested)
+                        tesnew = findViewById(R.id.sampletestednew)
+                        con.text = NumberFormat.getInstance().format(Integer.parseInt(confirmed))
+                        connew.text = ("+" + NumberFormat.getInstance().format(Integer.parseInt(confirmed_new)))
+                        rec.text = NumberFormat.getInstance().format(Integer.parseInt(recovered))
+                        recnew.text = ("+" + NumberFormat.getInstance().format(Integer.parseInt(recovered_new)))
+                        dea.text = NumberFormat.getInstance().format(Integer.parseInt(death))
+                        deanew.text = ("+" + NumberFormat.getInstance().format(Integer.parseInt(death_new)))
+                        tes.text = NumberFormat.getInstance().format(Integer.parseInt(tests))
+                        tesnew.text = ("+" + NumberFormat.getInstance().format(Integer.parseInt(tests_new)))
+                        act.text = NumberFormat.getInstance().format(Integer.parseInt(active))
+                        actnew.text = ("+" + NumberFormat.getInstance().format(Integer.parseInt(active_new)))
+                        date = findViewById(R.id.updateddate)
+                        tim = findViewById(R.id.updatedtime)
+                        date.text = Formatdate(time.toString(), 1)
+                        tim.text = Formatdate(time.toString(), 2)
+                        pieChart = findViewById(R.id.activity_main_piechart)
+                        pieChart.addPieSlice(PieModel("Active", Integer.parseInt(active).toFloat(), Color.parseColor("#007afe")))
+                        pieChart.addPieSlice(PieModel("Recovered", Integer.parseInt(recovered).toFloat(), Color.parseColor("#08a045")))
+                        pieChart.addPieSlice(PieModel("Deceased", Integer.parseInt(death).toFloat(), Color.parseColor("#F6404F")))
+                        pieChart.startAnimation()
+                                                   }, 1000)
 
+                },
+                { response ->
 
-                     },1000)
+                })
+            queue.add(jsonObjectRequest)
+        }
 
-             },
-            { response ->
+      @SuppressLint("SimpleDateFormat")
+         fun Formatdate(date:String, testcase:Int): String    {
+          val dateformat:String
+          val mdate =  SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US).parse(date)
+          if (testcase == 1){
+                dateformat = SimpleDateFormat("dd.MM.yyyy").format(mdate)
+                return dateformat
+          }
+          else{
+                 dateformat = SimpleDateFormat("hh:mm a").format(mdate)
+              return dateformat
+          }
+      }
 
-             })
-                            queue.add(jsonObjectRequest)
-                          }
 }
---
+++ app/src/main/java/com/example/covid/WorldDataActivity.kt
@@ -0,0 +1,106 @@
+package com.example.covid
+
+import android.graphics.Color
+import android.icu.text.NumberFormat
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.eazegraph.lib.charts.PieChart
+import org.eazegraph.lib.models.PieModel
+
+class WorldDataActivity : AppCompatActivity() {
+
+    private  lateinit var confirm_world:TextView
+    private lateinit var confirm_new_world:TextView
+    private  lateinit var active_world:TextView
+    private lateinit var active_new_world:TextView
+    private  lateinit var recovered_world:TextView
+    private lateinit var recovered_new_world:TextView
+    private  lateinit var death_world:TextView
+    private lateinit var death_new_world:TextView
+    private  lateinit var total_tests_world:TextView
+     private var conf_world: String? = null
+     private var conf_world_new:String? = null
+    private  var acti_world:String? = null
+    private var acti_world_new:String? = null
+    private var rec_world:String?=null
+    private var rec_world_new:String?=null
+    private var dea_world:String?=null
+    private var dea_world_new:String?=null
+    private var tottestworld:String? = null
+    private lateinit var pieChart: PieChart
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_world_data)
+        supportActionBar!!.title = "Covid-19 Tracker (World)"
+         FetchDataWorld()
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.activity_world_data_swipe_refresh_layout)
+            swipeRefreshLayout.setOnRefreshListener {
+                   FetchDataWorld()
+                   swipeRefreshLayout.isRefreshing = false
+            }
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun FetchDataWorld(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://corona.lmao.ninja/v2/all"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            { response ->
+                conf_world = response.getString("cases").toString()
+                conf_world_new = response.getString("todayCases").toString()
+                rec_world= response.getString("recovered").toString()
+                rec_world_new = response .getString("todayRecovered").toString()
+                dea_world = response.getString("deaths").toString()
+                dea_world_new = response.getString("todayDeaths").toString()
+                tottestworld = response.getString("tests").toString()
+                acti_world = response.getString("active").toString()
+                Handler().postDelayed({
+                    acti_world_new = (Integer.parseInt(conf_world_new) - (Integer.parseInt(dea_world_new) + Integer.parseInt(rec_world_new))).toString()
+                    confirm_world = findViewById(R.id.confirmworld)
+                    confirm_world.text = NumberFormat.getInstance().format(Integer.parseInt(conf_world))
+                    confirm_new_world = findViewById(R.id.confirmnewworld)
+                    confirm_new_world.text =("+"+ NumberFormat.getInstance().format(Integer.parseInt(conf_world_new)))
+                    active_world = findViewById(R.id.activeworld)
+                    active_world.text = NumberFormat.getInstance().format(Integer.parseInt(acti_world))
+                    active_new_world = findViewById(R.id.activenewworld)
+                    active_new_world.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(acti_world_new)))
+                    recovered_world = findViewById(R.id.recoveredworld)
+                    recovered_world.text = NumberFormat.getInstance().format(Integer.parseInt(rec_world))
+                    recovered_new_world = findViewById(R.id.recoverednewworld)
+                    recovered_new_world.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(rec_world_new)))
+                    death_world = findViewById(R.id.deathworld)
+                    death_world.text = NumberFormat.getInstance().format(Integer.parseInt(dea_world))
+                    death_new_world = findViewById(R.id.deathnewworld)
+                    death_new_world.text = ("+"+NumberFormat.getInstance().format(Integer.parseInt(dea_world_new)))
+                    total_tests_world = findViewById(R.id.totaltestworld)
+                     total_tests_world.text = NumberFormat.getInstance().format((tottestworld)!!.toLong())
+                    pieChart = findViewById(R.id.activity_world_data_piechart)
+                    pieChart.addPieSlice(PieModel("Active", Integer.parseInt(acti_world).toFloat(), Color.parseColor("#007afe")))
+                    pieChart.addPieSlice(PieModel("Recovered", Integer.parseInt(rec_world).toFloat(), Color.parseColor("#08a045")))
+                    pieChart.addPieSlice(PieModel("Deceased", Integer.parseInt(dea_world).toFloat(), Color.parseColor("#F6404F")))
+                    pieChart.startAnimation()
+
+                },1000)
+
+
+
+            },
+            { response ->
+
+
+            })
+        queue.add(jsonObjectRequest)
+    }
+
+}
